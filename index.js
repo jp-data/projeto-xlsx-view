327f32a567a062a64c8746c1ebb38f59a14c2a20
joao <joaopaulo94.economia@gmail.com>
2024-06-14 23:47:12 -0300

    Desafio completo

---
+++ index.js
@@ -0,0 +1,42 @@
+const excelJS = require('exceljs');
+// importação dos dados em json
+const dadosEmJson = require('./dados.json')
+
+
+// criação da pasta de trabalho
+const workbook =  new excelJS.Workbook();
+// add uma planilha nessa pasta
+const worksheet = workbook.addWorksheet('Resultados');
+
+// criando as colunas da planilha
+worksheet.columns = [
+    {header: 'Marca', key: 'marca', },
+    {header: 'Meta', key: 'meta', },
+    { header: 'Faturamento', key: 'faturamento'},
+    {header: 'Margem', key: 'margem', },
+]
+
+
+// separando os dados JSON em linhas de objetos
+const rows = dadosEmJson.marca.map((_, indice) => ({
+    marca: dadosEmJson.marca[indice],
+    meta: dadosEmJson.meta[indice],
+    faturamento: dadosEmJson.faturamento[indice],
+    margem: dadosEmJson.margem[indice]
+}));
+
+// adicionando as linhas na planilha
+worksheet.addRows(rows);
+// indices das colunas em negrito
+worksheet.getRow(1).font = {bold: true}
+
+
+// try e catch para tratar o assíncronismo e verificar possíveis erros
+workbook.xlsx.writeFile('resultados.xlsx')
+    .then(() => {
+        console.log('Planilha criada com sucesso!');
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+
